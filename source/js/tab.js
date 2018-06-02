c093389b3f22e761d78d1908a368142664426205
legiorex <batyushev@gmail.com>
2018-06-02 21:32:41 +1000

    Сделан таб на jquery

--- source/js/tab.js
+++ source/js/tab.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
-    $('.team__item').on('click',function (e) {
+    $('.team__link').on('click',function(e) {
+        e.preventDefault();
 
-        var teamTab = $('.team__list');
-        var ndx = $(this).index;
-        console.log(ndx);
-        $(this)
+        var
+            $this = $(this),
+            item = $this.closest('.team__item'),
+            container = $this.closest('.team'),
+            contentLeft = container.find('.team__text--fist'),
+            contentRight = container.find('.team__text'),
+            ndx = item.index();
+
+        item
             .addClass('active')
             .siblings()
             .removeClass('active');
+
+        contentRight.eq(ndx)
+            .removeClass('is-hidden')
+            .siblings()
+            .addClass('is-hidden');
+
+        contentLeft.eq(ndx)
+            .removeClass('is-hidden')
+            .siblings()
+            .addClass('is-hidden');
+
+
     });
 });
